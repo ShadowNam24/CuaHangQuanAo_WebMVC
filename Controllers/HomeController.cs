1f0982ef86a4571c90f3410069e5bcd72121978e
binkingisme <2200002704@nttu.edu.vn>
2025-08-22 14:21:20 +0700

    sửa lại trang home và update UI đăng nhập

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,47 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using CuaHangQuanAo.Models;
+using CuaHangQuanAo.Entities;            // <-- thêm
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;     // <-- thêm
 
 namespace CuaHangQuanAo.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CuaHangBanQuanAoContext _db;   // <-- thêm
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CuaHangBanQuanAoContext db) // <-- sửa ctor
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // NewProducts: lấy 8 món mới nhất (có thể sửa logic theo cột CreatedAt nếu có)
+            var newProducts = await _db.Items
+                .Include(i => i.Category)
+                .OrderByDescending(i => i.ItemsId)
+                .Take(8)
+                .ToListAsync();
+
+            // HotProducts: tạm thời lấy 8 món giá cao nhất (placeholder cho “bán chạy”)
+            var hotProducts = await _db.Items
+                .Include(i => i.Category)
+                .OrderByDescending(i => i.SellPrice)
+                .Take(8)
+                .ToListAsync();
+
+            var vm = new HomeIndexVm
+            {
+                NewProducts = newProducts,
+                HotProducts = hotProducts
+            };
+            return View(vm); // <-- truyền model
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Models/HomeIndexVm.cs
@@ -0,0 +1,8 @@
+﻿namespace CuaHangQuanAo.Entities
+{
+    public class HomeIndexVm
+    {
+        public IEnumerable<Item> NewProducts { get; set; } = Enumerable.Empty<Item>();
+        public IEnumerable<Item> HotProducts { get; set; } = Enumerable.Empty<Item>();
+    }
+}
