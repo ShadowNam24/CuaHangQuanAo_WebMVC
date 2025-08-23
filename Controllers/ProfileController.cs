d8ed26fdf9ed84f0e3818db01d2f2808dd12ebdf
Chuong185 <jonelamp81@gmail.com>
2025-08-23 20:42:01 +0700

    profile

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using CuaHangQuanAo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuaHangQuanAo.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private static Profile _profile = new Profile
+        {
+            FullName = "",
+            PhoneNumber = "",
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_profile);
+        }
+    }
+}
---
+++ Models/ProfileModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuaHangQuanAo.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; } = 1;
+
+        [Display(Name = "Họ Tên")]
+        [Required, StringLength(100)]
+        public string FullName { get; set; } = "";
+
+        [Display(Name = "Giới tính")]
+        public string Gender { get; set; } = "Nam"; // Nam/Nữ
+
+        [Display(Name = "Số điện thoại")]
+        [Phone]
+        public string PhoneNumber { get; set; } = "";
+
+        [Display(Name = "Địa chỉ")]
+        [StringLength(200)]
+        public string Address { get; set; } = "";
+
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        public bool EmailVerified { get; set; } = false;
+
+        // Ngày sinh (chia 3 trường cho dễ bind dropdown)
+        public int? BirthDay { get; set; }
+        public int? BirthMonth { get; set; }
+        public int? BirthYear { get; set; }
+
+        public string AvatarUrl { get; set; } = "/images/default-avatar.png";
+    }
+}
